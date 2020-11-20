9f974eb469b415726580282a9074af718817b279
DavidRich09 <luis.richmond9@gmail.com>
2020-11-19 19:25:36 -0600

    escenas
    Se agregó el cambio de escenar a unity

---
+++ Smash build a tree/Assets/Script/Scripts-UI/ManejoEscenas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManejoEscenas : MonoBehaviour
+{
+    public void CambiarNivel()
+    {
+
+        SceneManager.LoadScene("Escenario1");
+        Debug.Log("Me estriparon");
+    }
+}
