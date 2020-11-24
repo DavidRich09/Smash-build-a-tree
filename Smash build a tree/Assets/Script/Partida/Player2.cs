5d87887ad96b68e84d6e61a2a437c8d1ea90da4b
DavidRich09 <luis.richmond9@gmail.com>
2020-11-24 02:39:31 -0600

    Personajes
    Se agregaron 4 personajes extras

---
+++ Smash build a tree/Assets/Script/Partida/Player2.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    //Velocidad
+    public float speed = 2.5f;
+    public float salto = 5f;
+    
+
+    //Colisión suelo
+    public Transform verificaSuelo;
+    public LayerMask layerSuelo;
+    public float radioCheck;
+
+    //Referencias
+    private Rigidbody2D rigibody;
+    private Animator animator;
+
+    //Movemen
+    private Vector2 movimiento;
+    private bool verDerecha = true;
+    private bool inSuelo;
+
+    //Ataque
+    private bool ataque;
+
+    private void Awake()
+    {
+        rigibody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Movimiento
+        float entradahorizontal = Input.GetAxisRaw("Horizontal");
+        movimiento = new Vector2(entradahorizontal, 0f);
+
+        //Voltear
+        if (entradahorizontal < 0f && verDerecha == true)
+        {
+            Voltear();
+        }
+        else if (entradahorizontal > 0f && verDerecha == false)
+        {
+            Voltear();
+        }
+
+        //Esta en el suelo
+        inSuelo = Physics2D.OverlapCircle(verificaSuelo.position, radioCheck, layerSuelo);
+
+        if (Input.GetButtonDown("Jump") && inSuelo == true)
+        {
+            rigibody.AddForce(Vector2.up * salto, ForceMode2D.Impulse);
+        }
+
+        //Ataque
+        if (Input.GetButtonDown("Fire1") && inSuelo == true && ataque == false)
+        {
+            movimiento = Vector2.zero;
+            rigibody.velocity = Vector2.zero;
+            animator.SetTrigger("attack");
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float velhorizontal = movimiento.normalized.x * speed;
+        rigibody.velocity = new Vector2(velhorizontal, rigibody.velocity.y);
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetBool("idle", movimiento == Vector2.zero);
+        animator.SetBool("EsSuelo", inSuelo);
+        animator.SetFloat("Vvelocity", rigibody.velocity.y);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Attacking"))
+        {
+            ataque = true;
+        }
+        else
+        {
+            ataque = false;
+        }
+
+
+    }
+
+    private void Voltear()
+    {
+        verDerecha = !verDerecha;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+}
