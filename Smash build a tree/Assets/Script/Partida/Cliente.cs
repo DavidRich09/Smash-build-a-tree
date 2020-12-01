20fb46d14a752c07baf8a78f6d6975ceedb10bc6
DavidRich09 <luis.richmond9@gmail.com>
2020-11-30 21:37:42 -0600

    Conexion Cliente
    Se agregó la conexión de cliente de C#/unity a java

---
+++ Smash build a tree/Assets/Script/Partida/Cliente.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+public class Cliente : MonoBehaviour
+{
+    private Socket listen;
+    private IPEndPoint connect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+        Thread hiloCliente = new Thread(ClienteLogic);
+        hiloCliente.Start();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClienteLogic()
+    {
+
+        listen.Connect(connect);
+
+        while (true)
+        {
+            byte[] rcvLenBytes = new byte[4];
+            listen.Receive(rcvLenBytes);
+            int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+            byte[] rcvBytes = new byte[rcvLen];
+            listen.Receive(rcvBytes);
+            String recibido = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+            Debug.Log(recibido);
+
+        }
+
+
+    }
+
+    public void EnviarMensaje(string toSend)
+    {
+        int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+        byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+        byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+        listen.Send(toSendLenBytes);
+        listen.Send(toSendBytes);
+    }
+}
