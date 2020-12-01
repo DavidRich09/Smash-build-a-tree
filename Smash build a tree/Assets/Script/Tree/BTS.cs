f6bc0becff8e5a0431629bb9eb580f9a47feea18
DavidRich09 <luis.richmond9@gmail.com>
2020-12-01 02:33:36 -0600

    Arboles y opciones
    Se agregaron el arbol BTS y AVL, ademas del menu de opciones

---
+++ Smash build a tree/Assets/Script/Tree/BTS.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BTS : MonoBehaviour
+{
+    private GameObject[] nodos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodos = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            nodos[i] = transform.GetChild(i).gameObject;
+        }
+
+        foreach(GameObject nodo in nodos)
+        {
+            nodo.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void UpdateNodo(int[] nodosActivos)
+    {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+
+            nodos[i].SetActive(false);
+
+            if (i == nodosActivos[i])
+            {
+                nodos[i].SetActive(true);
+            }
+        }
+    }
+
+    public void ReiniciarArbol()
+    {
+        foreach(GameObject x in nodos)
+        {
+            x.SetActive(false);
+        }
+    }
+
+}
